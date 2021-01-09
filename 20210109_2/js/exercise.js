4377a843439b41fb351279a55da1b792201b7922
szbRebeka <szrebeka94@gmail.com>
2021-01-09 16:53:32 +0100

    completed 4 exercize out of 8

---
+++ 20210109_2/js/exercise.js
@@ -0,0 +1,40 @@
+let myVar=3*5;
+// legyen a változó értéke egy number
+myVar = Number.parseInt(myVar);
+console.log(myVar);
+// legyen a változó értéke egy sztring
+myVar = "3*5";
+console.log(myVar);
+// legyen a változó értéke egy boolean
+
+///* Juss el a "jég" szótól a "már"-ig úgy, hogy minden lépésben csak egy   betűt változtatsz meg! Minden lépésben adj új értéket a változónak és írasd ki a konzolra!
+//
+let wordgame = "jég";
+console.log(wordgame);
+let wordgame_2 = "még";
+console.log(wordgame_2)
+let wordgame_3 = "mág";
+console.log(wordgame_3)
+let wordgame_4 = "már";
+console.log(wordgame_4)
+
+// Írj egy programot, ami boldog születésnapot kíván valakinek! A nevet mentsd el egy változóba és irasd ki az üzenetet a konzolra!
+/*function myFunction() {
+    alert("Boldog szülinapot!");
+}
+let bday = alert("Boldog szülinapot!")
+console.log(bday) */
+
+// Miért fizetnél egy Jósnőnek, ha te magad is írhatsz egy programot, ami megmondja a jövőt?
+// Tárold az alábbi adatokat változókban: gyermekek száma, partner neve, város, foglalkozás
+// Írasd a konzolra az alábbi mondatot: "Te egy [foglalkozás] leszel [város]ban/ben, és [partner neve] lesz a párod, születni fog [gyermekek száma] gyereketek."
+/*let kids=window.prompt("Hány gyereked van?");
+let partner=window.prompt("Mi a partnered neve?");
+let city=window.prompt("Melyik városban laksz?");
+let job=window.prompt("Mi a foglalkozásod?");
+let result= "Te egy " + job + " " + "leszel" + " " + city + "-ban/ben, és" + " " + partner + " " + "lesz a párod, születni fog" + " " + kids + " " + "gyereketek.";
+console.log(result);
+alert(result);*/
+
+
+
