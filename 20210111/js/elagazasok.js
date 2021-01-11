0e146e7dd8fd227dd1f9c64680bbf1de03858ab7
szbRebeka <szrebeka94@gmail.com>
2021-01-11 20:18:19 +0100

    course-work_0111

---
+++ 20210111/js/elagazasok.js
@@ -0,0 +1,5 @@
+/*Írj egy programot, ami bekér be egy számot a felhasználótót  window.prompt-tal.
+Ha a bekért szám nagyobb 0-nál, akkor írja ki konzolra, hogy “a szám pozitív”
+ha a szám kisebb 0-nál írja ki, hogy “a szám negatív“”
+ha egyenlő 0-val, akkor írja ki: “nulla”*/
+let x = window.prompt("írj be egy számot!")
